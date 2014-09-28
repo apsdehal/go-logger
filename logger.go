11cedf65fc933e86170608b96db12f99c115e19b
Amanpreet Singh <apsdehal@gmail.com>
2014-09-28 19:00:13 +0530

    Adds comments for better explaination

--- logger.go
+++ logger.go
@@ -1,5 +1,7 @@
+// Package name declaration
 package logger
 
+// Import packages
 import (
  	"bytes"
  	"fmt"
@@ -7,10 +9,10 @@ import (
  	"log"
 )
 
-var colors []string
+// Contains color strings for stdout
+var colors map[string]int
 
-type color int
-
+// Color numbers for stdout
 const (
 	Black = (iota + 30)
 	Red
@@ -22,11 +24,16 @@ const (
 	White
 )
 
+// Worker class, Worker is a log object used to log messages and Color specifies
+// if colored output is to be produced
 type Worker struct {
 	Minion *log.Logger
 	Color bool
 }
 
+// Info class, Contains all the info on what has to logged, time is the current time, Module is the specific module
+// For which we are logging, level is the state, importance and type of message logged,
+// Message contains the string to be logged, format is the format of string to be passed to sprintf
 type Info struct {
 	Time time.Time
 	Module string
@@ -35,19 +42,26 @@ type Info struct {
 	format string
 }
 
+// Logger class that is an interface to user to log messages, Module is the module for which we are testing
+// worker is variable of Worker class that is used in bottom layers to log the message
 type Logger {
 	Module string
 	worker *Worker
 }
+
+// Returns a proper string to be outputted for a particular info
 func (r *Info) Output() string {
 	msg := fmt.Sprintf(r.format, r.Time, r.Level, r.message )
 	return msg
 }
 
+// Returns an instance of worker class, prefix is the string attached to every log, 
+// flag determine the log params, color parameters verifies whether we need colored outputs or not
 func NewWorker(prefix string, flag int, color bool) *Worker{
 	return &Worker{Minion: log.New(os.Stderr, prefix, flag), Color: color}
 }
 
+// Function of Worker class to log a string based on level
 func (l *Worker) Log(level string, calldepth int, info *Info) error {
 	if b.Color {
 		buf := &bytes.Buffer{}
@@ -60,10 +74,12 @@ func (l *Worker) Log(level string, calldepth int, info *Info) error {
 	}
 }
 
+// Returns a proper string to output for colored logging
 func colorString(color color) string {
 	return fmt.Sprintf("\033[%dm", int(color))
 }
 
+// Initializes the map of colors
 func initColors() {
 	colors = map[string]int{
 		"CRITICAL": colorString(Magenta),
@@ -74,12 +90,16 @@ func initColors() {
 	}
 }
 
+// Returns a new instance of logger class, module is the specific module for which we are logging
+// colors defines whether the output is to be colored or not
 func (*l Logger) New(module string, color bool) (*Logger, error) {
 	initColors()
 	newWorker := NewWorker("", 0, color)
 	return &Logger{Module: module, worker: newWorker}, nil	
 }
 
+// The log commnand is the function available to user to log message, lvl specifies
+// the degree of the messagethe user wants to log, message is the info user wants to log
 func (*l Logger) Log(lvl string, message string) {
 	var formatString string = "Some format here"
 	info := &Info{
@@ -92,36 +112,44 @@ func (*l Logger) Log(lvl string, message string) {
 	l.worker.log(lvl, 2, info)
 }
 
+// Fatal is just like func l,Critical logger except that it is followed by exit to program
 func (*l Logger) Fatal(message string) {
 	l.Log("CRITICAL", message)
 	os.Exit(1)
 }
 
+// Panic is just like func l.Critical except that it is followed by a call to panic
 func (*l Logger) Panic(message string) {
 	l.Log("CRITICAL", message)
 	panic(message)
 }
 
+// Critical logs a message at a Critical Level
 func (*l Logger) Critical(message string) {
 	l.Log("CRITICAL", message)
 }
 
+// Error logs a message at Error level
 func (*l Logger) Error(message string) {
 	l.Log("ERROR", message)
 }
 
+// Warning logs a message at Warning level
 func (*l Logger) Warning(message string) {
 	l.Log("WARNING", message)
 }
 
+// Notice logs a message at Notice level
 func (*l Logger) Notice(message string) {
 	l.Log("NOTICE", message)
 }
 
+// Info logs a message at Info level
 func (*l Logger) Info(message string) {
 	l.Log("INFO", message)
 }
 
+// Devug logs a message at Devug level
 func (*l Logger) Debug(message string) {
 	l.Log("DEBUG", message)
 }
